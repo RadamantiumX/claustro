22c226745ad3d5fb8be2fb024f12d20cd1f60ec6
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-05-27 21:38:53 -0300

    feat: adding method

--- loader/factory/src/repository/userColabRepository.ts
+++ loader/factory/src/repository/userColabRepository.ts
@@ -1,7 +1,14 @@
 import { prisma } from "../config/prismaClient";
+import { UserColab } from "factory";
 
 export class UserColabRepository{
-    async getUnique(){
+    async getUnique(username:Pick<UserColab, "username">):Promise<Pick<UserColab, "id" | "username" | "password" | "isSuperAdmin">>{
+       const unique = await prisma.userColab.findUnique({
+         where:{ username: username },
+         select: { id: true, username: true, password: true, isSuperAdmin: true }
+       })
+        
+     return unique
        
     }
 
