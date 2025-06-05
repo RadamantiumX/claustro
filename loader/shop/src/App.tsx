8c49f29ee9b8ba552b1e5fc1a5a38c908d9492f8
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-06-05 19:54:54 -0300

    feat: some change

--- loader/shop/src/App.tsx
+++ loader/shop/src/App.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {  QueryClientProvider } from '@tanstack/react-query'
+import { createTRPCClient, httpBatchLink } from '@trpc/client'
+import { useState } from 'react'
+import { TRPCProvider } from './utils/trpc'
+import { getQueryClient } from './config/queryClient'
+import type { AppRouter } from '../../factory/src/routers'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ const queryClient = getQueryClient()
+ const [trpcClient] = useState(()=>{
+    createTRPCClient<AppRouter>({
+      links: [
+        httpBatchLink({
+          url: 'http://localhost:3000/trpc'
+        }),
+      ]
+    })
+ })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <QueryClientProvider client={queryClient}>
+        <TRPCProvider trpcClient={trpcClient} queryClient={queryClient}>
+          <div className="">
+            Now is TRPC + REACTQUERY
+          </div>
+        </TRPCProvider>
+     </QueryClientProvider>
     </>
   )
 }
