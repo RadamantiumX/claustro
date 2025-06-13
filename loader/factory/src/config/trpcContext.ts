4c6adb64f6dc2ef07876ba4dd99a073ca8d84699
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-06-12 23:31:00 -0300

    feat: fixing type

--- loader/factory/src/config/trpcContext.ts
+++ loader/factory/src/config/trpcContext.ts
@@ -1,12 +1,13 @@
 import {  initTRPC, TRPCError } from '@trpc/server'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { AuthService } from '../services/authService'
+import { UserColab } from 'factory'
 
 
 
 
 // see the documentation
-export const createContext = ({ req, res }:trpcExpress.CreateExpressContextOptions) =>{
+export const createContext = async ({ req, res }:trpcExpress.CreateExpressContextOptions) =>{
     // JWT to improve
    const token:any = req.headers.authorization 
    try{
@@ -16,7 +17,7 @@ export const createContext = ({ req, res }:trpcExpress.CreateExpressContextOptio
     const authServiceInstance = AuthService.getInstance() // Access to intance
 
 
-    const verifyUser = authServiceInstance.auth.verifyCredentials(token)
+    const verifyUser:Pick<UserColab, "username" | "password" | "id" | "isSuperAdmin"> | null = await  authServiceInstance.auth.verifyCredentials(token)
     
 
     return { user: verifyUser }
