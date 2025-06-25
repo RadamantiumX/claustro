e8b212277c456baa814febafbdeeadb18732f065
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-06-25 19:55:22 -0300

    feat: latest changes

--- loader/shop/src/components/cards/BasicCard.tsx
+++ loader/shop/src/components/cards/BasicCard.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const BasicCard:React.FC<{children:React.ReactNode}> = ({children}) =>{
     return(
         <>
-        <div className="flex flex-col items-center p-10 gap-y-5 bg-gray-800 rounded-sm shadow-2xl">
+        <div className="card-basic">
            {children}
         </div>
         </>
