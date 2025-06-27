e35b7f0122bb91a3ddaabc3fb8a638ebbeae458a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-06-26 22:32:16 -0300

    feat: adding bg standar

--- loader/shop/src/components/buttons/CustomLink.tsx
+++ loader/shop/src/components/buttons/CustomLink.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 
 
 
-export const CustomLink:React.FC<{inner: string, route:string, color:string}> = ({inner, route, color}):JSX.Element => {
+export const CustomLink:React.FC<{inner: string, route:string}> = ({inner, route}):JSX.Element => {
   return (
-    <Link to={route} className={`rounded-sm bg-${color}-400 px-3 py-1 border-2 font-bold`}>
+    <Link to={route} className={`rounded-sm bg-purple-400 px-3 py-1 border-2 font-bold`}>
       {inner}
     </Link>
   )
--- loader/shop/src/features/guest/home/StartSection.tsx
+++ loader/shop/src/features/guest/home/StartSection.tsx
@@ -4,7 +4,7 @@ import { CustomLink } from "../../../components/buttons/CustomLink"
 export default function StartSection() {
   return (
     <GuestContent>
-      <CustomLink inner="Get Started" route="/signin" color="red"/>
+      <CustomLink inner="Get Started" route="/signin"/>
     </GuestContent>
   )
 }
