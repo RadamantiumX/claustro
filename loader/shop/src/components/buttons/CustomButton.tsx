cb114fa59b5bf236c24784493a7e0fe17ca4135d
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-06-26 23:24:37 -0300

    feat: new utilities for links and buttons

--- loader/shop/src/components/buttons/CustomButton.tsx
+++ loader/shop/src/components/buttons/CustomButton.tsx
@@ -4,7 +4,7 @@ type btn = "submit" | "reset" | "button" | undefined
 
 export const CustomButton:React.FC<{inner: string,  typeBtn:btn}> = ({inner, typeBtn})=> {
   return (
-    <button type={typeBtn} className={`rounded-sm bg-blue-400 px-3 py-1 border-2 font-bold cursor-pointer`}>
+    <button type={typeBtn} className={`blue-button`}>
       {inner}
     </button>
   )
--- loader/shop/src/components/buttons/CustomLink.tsx
+++ loader/shop/src/components/buttons/CustomLink.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 
 export const CustomLink:React.FC<{inner: string, route:string, fontSize:string}> = ({inner, route, fontSize}):JSX.Element => {
   return (
-    <Link to={route} className={`rounded-sm bg-purple-400 px-3 py-1 font-bold  ${fontSize}`}>
+    <Link to={route} className={`purple-link ${fontSize}`}>
       {inner}
     </Link>
   )
