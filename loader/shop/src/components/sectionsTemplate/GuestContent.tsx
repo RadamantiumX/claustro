f6b0adcb757c6244a23afa17fa146e03cea44614
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-06-30 19:59:20 -0300

    feat: first part of components comments

--- loader/shop/src/components/sectionsTemplate/GuestContent.tsx
+++ loader/shop/src/components/sectionsTemplate/GuestContent.tsx
@@ -1,5 +1,11 @@
 import React,{type ReactNode} from "react"
 
+/**
+ * Component sections, used only in the guest pages
+ * @component
+ * @param children React Node component go inside.
+ * @returns {React.ReactNode}
+ */
 const GuestContent:React.FC<{children:ReactNode}> = ({children}):React.ReactNode =>{
 return(
     <>
