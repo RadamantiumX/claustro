7a2186105eb6ed903e2b1f43e8909901396e4977
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-06-29 21:46:39 -0300

    refactor: clean any types

--- loader/shop/src/layouts/DefaultLayout.tsx
+++ loader/shop/src/layouts/DefaultLayout.tsx
@@ -4,8 +4,7 @@ import { Nav } from "../components/layoutItems/Nav"
 
 
 export default function DefaultLayout() {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const { token }:any = useStateContext()
+  const { token } = useStateContext()
   if(!token){
    return <Navigate to="/signin"/>
   }
--- loader/shop/src/layouts/GuestLayout.tsx
+++ loader/shop/src/layouts/GuestLayout.tsx
@@ -1,10 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Outlet, Navigate } from "react-router-dom"
 import { useStateContext } from "../shared/ContextProviders"
 import { Footer } from "../components/layoutItems/Footer"
 
 export default function GuestLayout() {
-  const { token }:any = useStateContext()
+  const { token } = useStateContext()
   if(token){
     return <Navigate to='/index'/>
   }
