c64ef6d6cdf27b5a19249972595206fca46f5f78
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-04 19:15:09 -0300

    feat: latest changes on project

---
+++ loader/shop/src/components/buttons/MenuButton.tsx
@@ -0,0 +1,11 @@
+import { Menu } from '../../../icons/Menu';
+
+export const MenuButton = () => {
+    return (
+        <>
+         <button className='cursor-pointer'>
+             <Menu />
+        </button>  
+        </>
+    )
+}
--- loader/shop/src/components/layoutItems/Nav.tsx
+++ loader/shop/src/components/layoutItems/Nav.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { LogoutButton } from "../buttons/LogoutButton"
 import { UserLink } from "../buttons/UserLink"
+import { MenuButton } from "../buttons/MenuButton"
 import { useStateContext } from "../../hooks/useCtxStates"
 
 /**
@@ -14,9 +15,11 @@ export const Nav = ():React.ReactNode =>{
     return(
         <>
         <nav className="flex justify-between items-center h-10 px-10 bg-gray-600">
-            <UserLink route="#" inner={`${user}`}/>
-            <LogoutButton/>
-            
+            <MenuButton/>
+            <div className="flex flex-row items-center gap-x-2">
+                 <UserLink route="#" inner={`${user}`}/>
+                 <LogoutButton/>
+            </div>
         </nav>
         </>
     )
