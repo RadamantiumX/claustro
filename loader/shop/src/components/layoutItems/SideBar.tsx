9e231dbb58280dd853b046de8b0d4f2376d2033d
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-04 23:33:40 -0300

    feat: adding hidden prop

--- loader/shop/src/components/layoutItems/SideBar.tsx
+++ loader/shop/src/components/layoutItems/SideBar.tsx
@@ -1,6 +1,10 @@
-export const SideBar = () => {
+import React from "react"
+import { useStateContext } from "../../hooks/useCtxStates"
+
+export const SideBar = ():React.ReactNode => {
+    const {over} = useStateContext()
     return(
-        <aside className="bg-amber-700 h-screen w-40 absolute hidden">
+        <aside className={over?`bg-amber-700 h-full w-40 absolute`:'hidden'}>
           
         </aside>
     )
