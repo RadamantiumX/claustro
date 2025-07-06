a47e475dd0fa9140d1dc60e1e0868b9176e41f0a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-05 22:28:18 -0300

    feat: any change

--- loader/shop/src/components/layoutItems/SideBar.tsx
+++ loader/shop/src/components/layoutItems/SideBar.tsx
@@ -4,7 +4,7 @@ import { useStateContext } from "../../hooks/useCtxStates"
 export const SideBar = ():React.ReactNode => {
     const {over} = useStateContext()
     return(
-        <aside className={over?`bg-amber-700 h-full w-40 transition delay-700 absolute`:'hidden'}>
+        <aside className={over?`bg-amber-700 h-1/2 w-40 transition delay-700 absolute`:'bg-amber-700 h-1/2 w-40 duration-1000 ease-in traslate-full'}>
           
         </aside>
     )
