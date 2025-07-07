c70b7e60a756b8b569c8da2c41f8d5c48f683b34
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-06 22:11:34 -0300

    feat: fixing traslation

--- loader/shop/src/components/layoutItems/SideBar.tsx
+++ loader/shop/src/components/layoutItems/SideBar.tsx
@@ -4,7 +4,7 @@ import { useStateContext } from "../../hooks/useCtxStates"
 export const SideBar = ():React.ReactNode => {
     const {over} = useStateContext()
     return(
-        <aside className={over?`bg-amber-700 h-1/2 w-40 transition translate-full delay-700 ease-in-out absolute`:'bg-amber-700 h-1/2 w-40 duration-1000 ease-in-out -traslate-full'}>
+        <aside className={over?`bg-amber-700 h-1/2 w-40 transition duration-1000 translate-1.5 delay-700 ease-in-out absolute`:'bg-amber-700 h-1/2 transition w-40 delay-700 ease-out'}>
           
         </aside>
     )
