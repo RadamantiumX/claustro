40b8400a9e511ca16fe6b248fded09743b8094d1
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-07 20:02:36 -0300

    feat: adding close button

--- loader/shop/src/components/buttons/MenuButton.tsx
+++ loader/shop/src/components/buttons/MenuButton.tsx
@@ -4,17 +4,13 @@ import { useStateContext } from '../../hooks/useCtxStates';
 export const MenuButton = () => {
    const {over, setOver} = useStateContext()
 
-   const onDalayMouseLeave = () =>{
-    setTimeout(()=>{
-        setOver(false)
-    },2000)
-   }
+  
 
     return (
         <>
-         <button className='cursor-pointer hover:rotate-95 ease-in-out duration-500' onMouseOver={()=>setOver(true)} onMouseLeave={onDalayMouseLeave}>
+         <button className='cursor-pointer hover:rotate-95 ease-in-out duration-500' onMouseOver={()=>setOver(true)}>
              <Menu mouseOver={over}/>
-        </button>  
+        </button>
         </>
     )
 }
--- loader/shop/src/components/layoutItems/SideBar.tsx
+++ loader/shop/src/components/layoutItems/SideBar.tsx
@@ -1,11 +1,14 @@
 import React from "react"
 import { useStateContext } from "../../hooks/useCtxStates"
+import { Close } from "../../icons/Close"
 
 export const SideBar = ():React.ReactNode => {
     const {over, setOver} = useStateContext()
     return(
-        <aside onMouseOver={()=>setOver(true)} onMouseLeave={()=>setOver(false)} className={over?`bg-amber-700 h-1/2 w-40 transition duration-1000 translate delay-700 ease-in-out absolute`:''}>
-          
+        <aside onMouseOver={()=>setOver(true)}  className={over?`bg-amber-700 h-1/2 w-40 transition duration-500 translate delay-300 ease-in-out absolute`:'bg-amber-700 h-1/2 w-40 transition duration-500 -translate-x-full delay-300 ease-in-out absolute'}>
+          <div className="flex justify-end m-2">
+            <button className="cursor-pointer" onClick={()=>setOver(false)}><Close/></button>
+          </div>
         </aside>
     )
 }
