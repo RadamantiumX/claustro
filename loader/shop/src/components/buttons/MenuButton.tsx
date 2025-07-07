d4abbe5bc96ecf29665825ae16cfada6762a3d1d
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-06 23:10:43 -0300

    feat: some changes

--- loader/shop/src/components/buttons/MenuButton.tsx
+++ loader/shop/src/components/buttons/MenuButton.tsx
@@ -4,9 +4,15 @@ import { useStateContext } from '../../hooks/useCtxStates';
 export const MenuButton = () => {
    const {over, setOver} = useStateContext()
 
+   const onDalayMouseLeave = () =>{
+    setTimeout(()=>{
+        setOver(false)
+    },2000)
+   }
+
     return (
         <>
-         <button className='cursor-pointer hover:rotate-95 ease-in-out duration-500' onMouseOver={()=>setOver(true)} onMouseLeave={()=>setOver(false)}>
+         <button className='cursor-pointer hover:rotate-95 ease-in-out duration-500' onMouseOver={()=>setOver(true)} onMouseLeave={onDalayMouseLeave}>
              <Menu mouseOver={over}/>
         </button>  
         </>
--- loader/shop/src/components/layoutItems/SideBar.tsx
+++ loader/shop/src/components/layoutItems/SideBar.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import { useStateContext } from "../../hooks/useCtxStates"
 
 export const SideBar = ():React.ReactNode => {
-    const {over} = useStateContext()
+    const {over, setOver} = useStateContext()
     return(
-        <aside className={over?`bg-amber-700 h-1/2 w-40 transition duration-1000 translate-1 delay-700 ease-in-out absolute`:''}>
+        <aside onMouseOver={()=>setOver(true)} onMouseLeave={()=>setOver(false)} className={over?`bg-amber-700 h-1/2 w-40 transition duration-1000 translate delay-700 ease-in-out absolute`:''}>
           
         </aside>
     )
