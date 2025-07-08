b9dece30791159eb648482ac7dbe819eae9cac66
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-07 21:49:02 -0300

    feat: adding hover and color

--- loader/shop/src/components/layoutItems/SideBar.tsx
+++ loader/shop/src/components/layoutItems/SideBar.tsx
@@ -6,8 +6,8 @@ export const SideBar = ():React.ReactNode => {
     const {over, setOver} = useStateContext()
     return(
         <aside onMouseOver={()=>setOver(true)}  className={over?`bg-amber-700 h-1/2 w-40 transition duration-500 translate delay-300 ease-in-out absolute`:'bg-amber-700 h-1/2 w-40 transition duration-500 -translate-x-full delay-300 ease-in-out absolute'}>
-          <div className="flex justify-end m-2">
-            <button className="cursor-pointer" onClick={()=>setOver(false)}><Close/></button>
+          <div onClick={()=>setOver(false)} className="flex justify-end m-2 cursor-pointer hover:text-yellow-400">
+               <Close/>
           </div>
         </aside>
     )
