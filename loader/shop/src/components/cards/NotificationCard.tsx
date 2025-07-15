f888b6981c0063084e270cbc3a600b31b78d6bdc
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-14 23:32:06 -0300

    feat: testing changes on bar

--- loader/shop/src/components/cards/NotificationCard.tsx
+++ loader/shop/src/components/cards/NotificationCard.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState, useEffect } from 'react'
 
 export const NotificationCard = () => {
+const [bar, setBar]:any = useState()
+
+ useEffect(()=>{
+setBar(document.getElementById('bar'))
+ },[])
+
   return (
-    <div className='flex flex-row justify-center'>
+    <div className='flex flex-col items-center'>
     <div className='flex flex-col justify-end absolute rounded-sm bg-blue-900 p-5'>
        <h4>This is a notification</h4> 
+       <div id="bar" className={`h-1 bg-blue-500`}></div>
+       
     </div>
+    
     </div>
   )
 }
