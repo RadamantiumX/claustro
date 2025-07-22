3c9a6b626e7856d0a513dfa6c705c0b16bdb6087
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-21 23:17:38 -0300

    feat: adding on style utilities

--- loader/shop/src/components/layoutItems/SideMenuBar.tsx
+++ loader/shop/src/components/layoutItems/SideMenuBar.tsx
@@ -5,7 +5,7 @@ import { Close } from "../../icons/icons"
 export const SideMenuBar = ():React.ReactNode => {
     const {over, setOver} = useStateContext()
     return(
-        <aside onMouseOver={()=>setOver(true)}  className={over?`menu-display`:'menu-hide'}>
+        <aside onMouseOver={()=>setOver(true)}  className={over?`side-menu-display`:'side-menu-hide'}>
           <div title="Close Menu" onClick={()=>setOver(false)} className="hover-close">
                <Close/>
           </div>
--- loader/shop/src/components/layoutItems/UserMenuBar.tsx
+++ loader/shop/src/components/layoutItems/UserMenuBar.tsx
@@ -4,7 +4,7 @@ export const UserMenuBar = () => {
     const {show} = useStateContext()
   return (
     <div>
-        <nav className={!show ? 'h-1/2 w-40 transition -translate-y-full bg-amber-800 top-10 right-0 ease-in-out absolute':'h-1/2 w-40 transition bg-amber-800 top-10 right-0 ease-in-out absolute'}>
+        <nav className={!show ? 'user-menu-hide':'user-menu-display'}>
 
        </nav>
     </div>
