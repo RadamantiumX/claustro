1846f3cf9cfd5e17f73be823a48e057b62f0c8c7
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-22 22:23:43 -0300

    feat: some changes

--- loader/shop/src/components/headers/SearchInput.tsx
+++ loader/shop/src/components/headers/SearchInput.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { MagnifyGlass } from '../../icons/icons'
 
-export const SearchInput = () => {
+export const SearchInput = ():React.ReactNode => {
   return (
     <>
-     <input type="search" className='w-[70%] h-16 p-3 rounded mb-8 italic text-gray-600 bg-amber-100 focus:outline-none focus:shadow-outline text-xl shadow-lg' placeholder='Search Record'/>
+    <div className='realative w-full flex justify-center'>
+     <input type="search" className='w-[70%] h-10 p-3 rounded mb-8 italic text-gray-600 bg-amber-100 focus:outline-none focus:shadow-outline text-xl shadow-lg' placeholder='Search Record'/>
+     <div className='flex absolute'><MagnifyGlass/></div>
+     </div>
     </>
   )
 }
--- loader/shop/src/icons/icons.tsx
+++ loader/shop/src/icons/icons.tsx
@@ -3,4 +3,5 @@ export { Close } from './Close';
 export { Menu } from './Menu';
 export { OffPower } from './OffPower';
 export { Loader } from './Loader';
+export { MagnifyGlass } from './MagnifyGlass'
 
