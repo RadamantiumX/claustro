e65122e8dd3348ff6b8142ad1b6a877ad8bbd9b6
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-24 20:03:09 -0300

    feat: setting data repository

--- loader/factory/src/repository/dataRepository.ts
+++ loader/factory/src/repository/dataRepository.ts
@@ -1,17 +1,110 @@
+import { prisma } from "../config/prismaClient";
+import { Datum } from "factory";
+import { timeStampParsed } from "../helper/timeStampParser";
+
 export class DataRepository{
-    async getUnique(){
+    async getUnique({id}:Pick<Datum, "id">){
+       const unique = prisma.data.findUnique({
+        where:{ id: id },
+        select:{
+            id: true,
+            emailSource:true,
+            xUser: true,
+            apiData: {
+                select: {
+                    appName: true,
+                    appId: true
+                }
+            },
+            apiKeys: {
+                select: {
+                    apiKey: true,
+                    apiKeySecret: true,
+                    bearerToken: true,
+                    accessToken: true,
+                    accessTokenSecret: true
+                }
+            }
+        }
+    })
 
+    return unique
     }
 
-    async createData(){
+    async getForEmailSource({emailSource}:Pick<Datum, "emailSource">){
+       const unique = prisma.data.findUnique({
+        where:{ emailSource: emailSource },
+        select:{
+            id: true,
+            emailSource:true,
+            xUser: true,
+            apiData: {
+                select: {
+                    appName: true,
+                    appId: true
+                }
+            },
+            apiKeys: {
+                select: {
+                    apiKey: true,
+                    apiKeySecret: true,
+                    bearerToken: true,
+                    accessToken: true,
+                    accessTokenSecret: true
+                }
+            }
+        }
+    })
 
+    return unique
+    }
+
+    async allData(){
+        const allDataRecords = await prisma.data.findMany({
+            select:{
+                id: true,
+                emailSource: true,
+                xUser: true,
+                userColabId: true, 
+                createdAt: true
+            }
+        })
+        return allDataRecords
+    }
+
+    async createData(payload:Omit<Datum, "id" | "createdAt" | "updatedAt">){
+       await prisma.data.create({
+        data: {
+            emailSource: payload.emailSource,
+            emailSourcePsw: payload.emailSourcePsw,
+            xUser: payload.xUser,
+            xPsw: payload.xPsw,
+            userColabId: payload.userColabId
+        }
+       })
+       return
     }
     
-    async updateData(){
+    async updateData(payload:Omit<Datum, "createdAt" | "updatedAt">){
+      await prisma.data.update({
+      where: {
+        id: payload.id
+      },
+      data: {
+        emailSource: payload.emailSource,
+        emailSourcePsw: payload.emailSourcePsw,
+        xUser: payload.xUser,
+        xPsw: payload.xPsw,
+        userColabId: payload.userColabId,
+        updatedAt: timeStampParsed()
+      }
+    })
 
+    return
     }
     
-    async destroyData(){
-
+    async destroyData(payload: Pick<Datum, 'id'>){
+       await prisma.data.delete({ where: { id: payload.id } })
+       return
     }
 }
