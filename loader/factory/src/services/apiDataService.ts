ae0e339a3b115a8b3894776071f5fcadbcf91180
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-27 21:38:26 -0300

    feat: new service arrives

--- loader/factory/src/services/apiDataService.ts
+++ loader/factory/src/services/apiDataService.ts
@@ -1,9 +1,22 @@
 import { IApiDataRepository, ApiDataMethods } from "index"
+import { ApiDataRepository } from "../repository/apiDataRepository";
+
 
 export class ApiDataService{
     private static instance:ApiDataService
     apiDataRepository:IApiDataRepository;
     apiData:ApiDataMethods;
 
-    
+    private constructor(apiDataRepository:IApiDataRepository){
+        this.apiDataRepository = this.apiDataRepository
+        this.apiData = {
+
+        }
+    }
+
+    static getInstance(){
+        if(!ApiDataService.instance){
+            ApiDataService.instance = new ApiDataService(new ApiDataRepository)
+        }
+    }
 }
