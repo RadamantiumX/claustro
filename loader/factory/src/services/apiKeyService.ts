8509dcdf8b332f12fc95da426ebcd5d890f6cf0a
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-28 19:44:26 -0300

    feat: adding class prop methods

--- loader/factory/src/services/apiKeyService.ts
+++ loader/factory/src/services/apiKeyService.ts
@@ -1,15 +1,30 @@
-import { IApiKeyRepository, ApiKeyMethods } from "index";
+import { IApiKeyRepository, ApiKeyMethods, ApiKey } from "index";
 import { ApiKeyRepository } from "../repository/ApiKeyRepository";
 
 export class ApiKeyService{
-    private static intance:ApiKeyService;
+    private static instance:ApiKeyService;
     apiKeyRepository:IApiKeyRepository;
     apiKey:ApiKeyMethods;
 
     private constructor(apiKeyRepository:IApiKeyRepository){
         this.apiKeyRepository = this.apiKeyRepository;
         this.apiKey = {
-
+                  create: async(bodyReq:Omit<ApiKey, "id" | "updatedAt" | "createdAt">)=>{
+                    await this.apiKeyRepository.createApiKey(bodyReq)
+                    return
+                  },
+                  selectUniqueForId: async(bodyReq:Pick<ApiKey, "id">)=>{
+                    const uniqueForId = await this.apiKeyRepository.getUnique(bodyReq)
+                    return uniqueForId
+                  },
+                  update: async(bodyReq:Omit<ApiKey, "updatedAt" | "createdAt" | "dataId" | "apiDataId">)=>{
+                    await this.apiKeyRepository.updateApiKey(bodyReq)
+                    return
+                  },
+                  delete: async(bodyReq:Pick<ApiKey, "id">)=>{
+                    await this.apiKeyRepository.destroyApiKey(bodyReq)
+                    return
+                  }
         }
     }
 
