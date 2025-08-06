8747e5928caff256ae378b8dfd9465f75dc0728b
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-08-05 23:05:56 -0300

    feat: some changes

--- loader/factory/src/config/prismaClient.ts
+++ loader/factory/src/config/prismaClient.ts
@@ -8,4 +8,14 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
+// TODO: handle al env variable to: TEST, DEV, PRODUCTION
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+if (process.env.NODE_ENV === 'test'){
+    // TODO: only for TEST
+   // prisma = {} as PrismaClient 
+}else{
+    // TODO: only for DEV
+
+    // TODO: only for PROD
+}
--- loader/factory/vitest.config.ts
+++ loader/factory/vitest.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitest/config'
-
+// Can add the "setUpFiles" property, for include another file with the "MOCK" configuration of Data Base.
 export default defineConfig({
   test: {
     include: ['**/*.test.ts', '**/*.spec.ts'],
