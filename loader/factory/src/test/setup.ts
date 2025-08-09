022f38d547c2f6690c1c837c3047bdc9222162d4
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-08-08 23:30:55 -0300

    feat: setup for singleton prisma client

---
+++ loader/factory/src/test/setup.ts
@@ -0,0 +1,18 @@
+// Singleton SETUP
+import { mockReset, mockDeep, DeepMockProxy } from 'vitest-mock-extended';
+import { PrismaClient } from '@prisma/client';
+import { vi, beforeEach } from 'vitest';
+import prisma from '../config/prismaClient';
+
+vi.mock('../config/prismaClient', ()=>{
+   return{
+      __esModule: true,
+      default: mockDeep<PrismaClient>
+   }
+})
+
+beforeEach(()=>{
+    mockReset(prismaMock)
+})
+
+export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
