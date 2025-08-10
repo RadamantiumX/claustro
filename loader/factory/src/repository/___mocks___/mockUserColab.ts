a67fe5fc10e43d5ffdfe07da48aa5cfe695757d1
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-08-10 20:06:09 -0300

    feat: type correction

--- loader/factory/src/repository/___mocks___/mockUserColab.ts
+++ loader/factory/src/repository/___mocks___/mockUserColab.ts
@@ -2,13 +2,21 @@ import { UserColabRepository } from "../userColabRepository";
 import { prismaMock } from "../../test/setup";
 import { it, vi, expect } from "vitest";
 
+
 const userColabRepository = new UserColabRepository()
 
 it('sould create new user', async () => {
    
    const userColab = {
+    id:'asas',  
     username: 'usecolabmock',
-    password: 'testingmock123'
+    email: "user@mock.com",
+    password: 'testingmock123',
+    lastSignIn: new Date(Date.now()),
+    createdAt: new Date(Date.now()),
+    updatedAt: new Date(Date.now()),
+    isSuperAdmin: false,
+
    }
 
    prismaMock.userColab.create.mockResolvedValue(userColab)
