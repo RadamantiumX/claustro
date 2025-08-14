d618c72000af096faea5a6c4fc5c49141d121240
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-08-14 11:26:32 -0300

    feat: test update

--- loader/factory/src/test/unit/userColab.unit.test.ts
+++ loader/factory/src/test/unit/userColab.unit.test.ts
@@ -1,7 +1,7 @@
 import { UserColabRepository } from "../../repository/userColabRepository";
 import { prismaMock } from "../setup";
 import { it, vi, expect, expectTypeOf, beforeEach, describe } from "vitest";
-
+import { timeStampParsed } from "../../helper/timeStampParser";
 
 /**
  * Testing REPOSITORIES FILES
@@ -15,6 +15,7 @@ describe('UserColabRepository',()=>{
    userColabRepository = new UserColabRepository(prismaMock)
 })
 
+// UserColab MOCKED
 const mockUserColab = {
     id:'asas',  
     username: 'usecolabmock',
@@ -32,6 +33,8 @@ it('should create new userColab',async ()=>{
    const newUser = await userColabRepository.createUserColab({username: 'usecolabmock', password: 'testingmock123'})
    
    expect(newUser).toEqual(undefined) // <-- Mocking VOID behavior
+
+   // Mocking Prisma Properties
    expect(prismaMock.userColab.create).toHaveBeenCalledWith({
       data: {
          username: 'usecolabmock',
@@ -41,22 +44,38 @@ it('should create new userColab',async ()=>{
 })
 
 it('should return unique UserColab',async ()=>{
-   const mockUserColab = {
-    id:'asas',  
-    username: 'usecolabmock',
-    email: "user@mock.com",
-    password: 'testingmock123',
-    lastSignIn: new Date(Date.now()),
-    createdAt: new Date(Date.now()),
-    updatedAt: new Date(Date.now()),
-    isSuperAdmin: false,
-   }
-
    prismaMock.userColab.findUnique.mockResolvedValueOnce(mockUserColab)
    const newUser = await userColabRepository.getUnique({username:'usecolabmock'})
-   
+
    expect(newUser).toEqual(expect.objectContaining({id:'asas',username: 'usecolabmock', password:'testingmock123', isSuperAdmin: false}))
-  
+
+   // Mocking Prisma Properties
+   expect(prismaMock.userColab.findUnique).toHaveBeenCalledWith({
+      where:{
+         username:'usecolabmock'
+      },
+      select:{ id: true, username: true, password: true, isSuperAdmin: true }
+   })
+})
+
+it('should update UserColab',async ()=>{
+   prismaMock.userColab.update.mockResolvedValueOnce(mockUserColab)
+   const newUser = await userColabRepository.updateUserColab({id:'asas',username:'usecolabmock', password:'testingmock123',isSuperAdmin:false})
+
+   expect(newUser).toEqual(undefined) // <-- Mocking VOID behavior
+
+   // Mocking Prisma Properties
+   expect(prismaMock.userColab.update).toHaveBeenCalledWith({
+      where:{
+         id:'asas'
+      },
+      data:{
+         username: 'usecolabmock',
+         password: 'testingmock123',
+         isSuperAdmin: false,
+         updatedAt: timeStampParsed()
+      }
+   })
 })
 
 })
