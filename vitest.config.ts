3848e10c2d784d866b3d32c9cf85b6baab71120d
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-08-23 00:43:34 -0300

    feat: adding some midnigth changes

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config'
+// Can add the "setUpFiles" property, for include another file with the "MOCK" configuration of Data Base.
+export default defineConfig({
+  test: {
+    include: ['**/*.test.ts', '**/*.spec.ts'],
+    includeSource: ['src/**/*.{js, ts}'],
+    globals: true,
+    environment: 'node',
+    setupFiles: ["./src/test/setup.ts"],
+    isolate: false,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html']
+    }
+  },
+  define: { 
+    'import.meta.vitest': 'undefined', 
+  }, 
+  
+})
