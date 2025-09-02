85e3ae5ae847cab7af2e77863b809bf02122b8ba
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-09-02 19:56:08 -0300

    feat: fixing cookies set on server

--- loader/factory/src/lib/trpcContext.ts
+++ loader/factory/src/lib/trpcContext.ts
@@ -1,37 +1,41 @@
 import { initTRPC } from '@trpc/server'
 import * as trpcExpress from '@trpc/server/adapters/express'
-import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import { AuthService } from '../services/authService'
 import { type UserColab } from '../declarations/index'
 import { JWTtokenSign } from '../helper/jwtFunctions'
 import { R_TOKEN_EXP } from '../const/tokenExpiration'
-
+import { TRPCError } from '@trpc/server'
 
 
 // TODO: put inside this context other functions with other context. EXAMPLE: THE REFRESH TOKEN
 
+interface CreateContextOptions {
+    req: trpcExpress.CreateExpressContextOptions['req'];
+    res: trpcExpress.CreateExpressContextOptions['res'];
+    verifiedUser: Pick<UserColab, "username" | "password" | "id" | "isSuperAdmin"> | null;
+}
 // see the documentation
-export const createContext = async ({ req, res, }:trpcExpress.CreateExpressContextOptions) =>{
+export const createContext = async ({ req, res, }:trpcExpress.CreateExpressContextOptions):Promise<CreateContextOptions> =>{
     // TODO: Adding security on "private procedure" middleware
     // JWT to improve
-   /*const token:any = req.headers.authorization 
-    if(!token){
-        return null
-    }
+   const token:any = req.headers.authorization 
+   
     const authServiceInstance = AuthService.getInstance() // Access to intance
 
-    const verifyUser:Pick<UserColab, "username" | "password" | "id" | "isSuperAdmin"> | null = await  authServiceInstance.auth.verifyCredentials(token)
-
-    if(!verifyUser){
-        return null
+    const verifiedUser:Pick<UserColab, "username" | "password" | "id" | "isSuperAdmin"> | null = await  authServiceInstance.auth.verifyCredentials(token)
+    if(!verifiedUser || !token){
+        throw new TRPCError({
+            code: 'UNAUTHORIZED',
+            message: 'You are not authorized from the server'
+        })
     }
      const refreshToken = JWTtokenSign({
-                id: verifyUser.id,
-                username: verifyUser.username,
-                isSuperAdmin: verifyUser.isSuperAdmin,
+                id: verifiedUser.id,
+                username: verifiedUser.username,
+                isSuperAdmin: verifiedUser.isSuperAdmin,
                 expiresIn: R_TOKEN_EXP
-             })*/
-    return { req, res }
+             })
+    return { req, res, verifiedUser }
   
 }
 
