d1cd31c095ea38de6a96f20508d232363a29d159
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-09-03 18:04:29 -0300

    feat: adding new repo

--- loader/factory/src/repository/refreshTokenRepository.ts
+++ loader/factory/src/repository/refreshTokenRepository.ts
@@ -1,2 +1,11 @@
 // TODO: Send to CLIENT the ACCESS TOKEN refreshed
 // For the moment, it's not necessary the DB
+import { PrismaClient } from "@prisma/client";
+import type { AuthRefreshToken } from "../declarations/index";
+export class RefreshTokenRepository {
+    constructor(private prismaClient:PrismaClient){}
+    
+    async createRefeshToken(payload:){
+        
+    }
+}
