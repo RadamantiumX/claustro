bcdd9984c38f37c7d97e0896bf254ba9df89d2bb
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-09-03 19:01:27 -0300

    feat: refresh token setting

--- loader/factory/src/repository/refreshTokenRepository.ts
+++ loader/factory/src/repository/refreshTokenRepository.ts
@@ -1,11 +1,17 @@
 // TODO: Send to CLIENT the ACCESS TOKEN refreshed
 // For the moment, it's not necessary the DB
 import { PrismaClient } from "@prisma/client";
-import type { AuthRefreshToken } from "../declarations/index";
+import type { AuthRefreshToken, PayloadRefreshToken } from "../declarations/index";
 export class RefreshTokenRepository {
     constructor(private prismaClient:PrismaClient){}
     
-    async createRefeshToken(payload:){
-        
+    async createRefeshToken(payload:PayloadRefreshToken){
+        await this.prismaClient.authRefreshToken.create({
+            data:{
+                refreshToken: payload.refreshToken,
+                userColabId: payload.userColabId
+            }
+        })
+        return
     }
 }
