f5bed6c8becfb47a821fc474e251b6f58a8bb0c1
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-09-04 15:50:15 -0300

    feat: new class service setting

--- loader/factory/src/services/refreshTokenService.ts
+++ loader/factory/src/services/refreshTokenService.ts
@@ -1,5 +1,19 @@
-import type { IRefreshTokenRepository } from "../declarations/serviceInjection";
+import type { IRefreshTokenRepository } from "../declarations/index";
+import { RefreshTokenRepository } from "../repository/refreshTokenRepository";
+import prisma from "../config/prismaClient"; 
 
 export class RefreshTokenService{
-    
+    private static instance:RefreshTokenService
+    refreshTokenRepository:IRefreshTokenRepository;
+    private constructor(refreshTokenRepository:IRefreshTokenRepository){
+        
+    }
+    static getInstance(){
+            if(!RefreshTokenService.instance){
+               RefreshTokenService.instance = new RefreshTokenService(new RefreshTokenRepository(prisma))
+               console.log('Service userColab ONLINE')
+               
+           }
+           return RefreshTokenService.instance
+    }
 }
