a5985b7e97f2f76d20853f530581960b24a1e2be
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-09-10 17:43:21 -0300

    feat: adding exp constants

--- loader/factory/src/routers/refreshTokenRouter.ts
+++ loader/factory/src/routers/refreshTokenRouter.ts
@@ -1,14 +1,16 @@
 import { trpc } from "../lib/trpcContext";
-import { refreshTokenSchema } from "../schemas/zodSchemas/refreshTokenValidation";
 import { RefreshTokenService } from "../services/refreshTokenService";
 import { UserColabService } from "../services/userColabService";
 import { protectedProcedure } from "../lib/procedure";
 import { TRPCError } from "@trpc/server";
 import { JWTtokenSign } from "../helper/jwtFunctions";
+import { A_TOKEN_EXP, R_TOKEN_EXP } from "../const/tokenExpiration";
 
 const refreshTokenInstance = RefreshTokenService.getInstance()
 const userColabInstance = UserColabService.getInstance()
 
+
+// Refresh TOKEN route
 export const refreshTokenRouter = trpc.router({
     refresh: protectedProcedure.query(async({ctx})=>{
         const cookies =  ctx.req.cookies
@@ -22,8 +24,8 @@ export const refreshTokenRouter = trpc.router({
         if(!verify){
             throw new TRPCError({ code: 'UNAUTHORIZED' })
         }
-        const newAt = JWTtokenSign({ id:verify.id, username:verify.username, isSuperAdmin: verify.isSuperAdmin, expiresIn:'6s' })
-        const newRt = JWTtokenSign({ id:verify.id, username:verify.username, isSuperAdmin: verify.isSuperAdmin, expiresIn:'1h' })
+        const newAt = JWTtokenSign({ id:verify.id, username:verify.username, isSuperAdmin: verify.isSuperAdmin, expiresIn: A_TOKEN_EXP })
+        const newRt = JWTtokenSign({ id:verify.id, username:verify.username, isSuperAdmin: verify.isSuperAdmin, expiresIn: R_TOKEN_EXP })
         ctx.res.cookie('jwt', newRt, { httpOnly: true, secure: true, maxAge: 24 * 60 * 60 * 1000 })
         return newAt
     })
