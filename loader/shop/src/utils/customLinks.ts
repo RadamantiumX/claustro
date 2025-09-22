266a8b16c5c6b021b5d4fc0165734a2711f11e36
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-09-22 17:54:38 -0300

    feat: new change

--- loader/shop/src/utils/customLinks.ts
+++ loader/shop/src/utils/customLinks.ts
@@ -1,11 +1,18 @@
 import type{ TRPCLink } from "@trpc/client";
 import type { AppRouter } from "../../../factory/src/routers";
 import { useStateContext } from "../hooks/useCtxStates";
-
+import { isExpiredToken } from "../helper/tokenExpiration";
 // TODO: adding expiration token conditional
 
 const accessTokenLink: TRPCLink<AppRouter> = (runtime) => (ctx) =>{
   return (next) => async (op) =>{
     const { token, setToken } = useStateContext()
+
+    if(token){
+        if(isExpiredToken(token)){
+          
+        }
+    }
+
   }
 }
