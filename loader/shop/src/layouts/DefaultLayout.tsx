6198578f8d535fcb879bc2072d9b2763927ad41f
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-09-24 15:35:45 -0300

    feat: some change

--- loader/shop/src/layouts/DefaultLayout.tsx
+++ loader/shop/src/layouts/DefaultLayout.tsx
@@ -1,11 +1,9 @@
 import { Outlet, Navigate } from "react-router-dom"
 import { useStateContext } from "../hooks/useCtxStates"
-import { useEffect } from "react"
 import { Nav } from "../components/layoutItems/Nav"
 import { SideMenuBar } from "../components/layoutItems/SideMenuBar"
 import { UserMenuBar } from "../components/layoutItems/UserMenuBar"
 import { Footer } from "../components/layoutItems/Footer"
-import { jwtDecode } from "jwt-decode"
 import React from "react"
 import { useCloseBar } from "../hooks/useCloseBar"
 
@@ -18,18 +16,7 @@ export default function DefaultLayout():React.ReactNode {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { token }:any = useStateContext()
   
-  if(token !== null){
-    const decoded:any = jwtDecode(token)
-    const verifyToken = () => {
-    const currentTime = Date.now() /1000
-    console.log(`exp: ${decoded.exp}`)
-    console.log(`current time: ${currentTime}`)
-    return decoded?.exp < currentTime;
-  }
-  if(verifyToken){
-    console.log('The token it\'s expired')
-  }
-  }
+  
  
   if(!token){
    return <Navigate to="/signin"/>
