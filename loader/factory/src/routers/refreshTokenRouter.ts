770349caeff0e260dd86139a9f95c708cc50c1c2
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-09-26 14:41:56 -0300

    feat: change to mutation and adding db op

--- loader/factory/src/routers/refreshTokenRouter.ts
+++ loader/factory/src/routers/refreshTokenRouter.ts
@@ -6,6 +6,7 @@ import { TRPCError } from "@trpc/server";
 import { JWTtokenSign } from "../helper/jwtFunctions";
 import { A_TOKEN_EXP, R_TOKEN_EXP } from "../const/tokenExpiration";
 import { COOKIE_AGE } from "../const/cookieAge";
+import { refreshTokenSchema } from "../schemas/zodSchemas/refreshTokenValidation";
 
 // Services Instances
 const refreshTokenInstance = RefreshTokenService.getInstance()
@@ -14,11 +15,11 @@ const userColabInstance = UserColabService.getInstance()
 
 // Refresh TOKEN route
 export const refreshTokenRouter = trpc.router({
-    refresh: protectedProcedure.query(async({ctx})=>{
+    refresh: protectedProcedure.input(refreshTokenSchema.pick({userColabId: true})).mutation(async({ctx, input})=>{
         const cookies =  ctx.req.cookies
         const refreshToken:string = cookies.jwt
         ctx.res.clearCookie('jwt', { httpOnly: true, secure: true })
-        const owner = await refreshTokenInstance.refreshToken.verifyOwner(refreshToken)
+        const owner = await refreshTokenInstance.refreshToken.verifyOwner({userColabId:input.userColabId, refreshToken: refreshToken})
         if(!owner){
             throw new TRPCError({ code: 'UNAUTHORIZED', message: 'The token provided is invalid' })
         }
@@ -28,6 +29,9 @@ export const refreshTokenRouter = trpc.router({
         }
         const newAccessToken = JWTtokenSign({ id:verify.id, username:verify.username, isSuperAdmin: verify.isSuperAdmin, expiresIn: A_TOKEN_EXP })
         const newRefreshToken = JWTtokenSign({ id:verify.id, username:verify.username, isSuperAdmin: verify.isSuperAdmin, expiresIn: R_TOKEN_EXP })
+
+        await refreshTokenInstance.refreshTokenRepository.updateRefreshToken({userColabId: input.userColabId, refreshToken: newRefreshToken}) // Update the new REFRESH TOKEN
+
         ctx.res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, maxAge: COOKIE_AGE })
         return newAccessToken
     })
