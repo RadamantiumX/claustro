86ff326a9532a676fd66f7b2a625fee9c9ed87d6
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-10-01 23:00:30 -0300

    feat: hook for fetching the data of trpc server

--- loader/shop/src/hooks/useFetchData.ts
+++ loader/shop/src/hooks/useFetchData.ts
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useTRPC } from "../utils/trpc";
 
 export const useFetchData = () => {
     const trpc = useTRPC()
-    const [data, setData] = useState([])
-
     const allData = useQuery(trpc.data.list.queryOptions())
-
-    setData([allData,...data])
-    return {data}
+   const data = allData.data
+  return {data}
 }
