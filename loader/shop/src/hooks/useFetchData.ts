ffdfdb2fe1b8945187eddf8ad3b4df8eb6a47ac8
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-10-13 15:57:29 -0300

    feat: some changes

--- loader/shop/src/hooks/useFetchData.ts
+++ loader/shop/src/hooks/useFetchData.ts
@@ -1,16 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { useTRPC } from "../utils/trpc";
-import { useState } from "react";
+
 
 export const useFetchData = () => {
     const trpc = useTRPC()
-    const [loading, setLoading] = useState(false)
+    
     const allData = useQuery(trpc.data.list.queryOptions())
-    if(allData.isLoading){
-      setLoading(true)
-    }
-    setLoading(false)
-   console.log('Finish Data loading...') 
+   
+   
    const data = allData.data
-  return {data, loading}
+  return {data}
 }
