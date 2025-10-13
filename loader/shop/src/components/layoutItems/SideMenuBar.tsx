2643672e64eaa30c19bc0ec69e226ef36405d9d0
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-10-13 19:59:39 -0300

    feat: some change

--- loader/shop/src/components/layoutItems/SideMenuBar.tsx
+++ loader/shop/src/components/layoutItems/SideMenuBar.tsx
@@ -1,17 +1,19 @@
 import React from "react"
 import { useStateContext } from "../../hooks/hooks"
 import { Close } from "../../icons/icons"
+import { CustomLink } from "../buttons/CustomLink"
 
 export const SideMenuBar = ():React.ReactNode => {
     const {over, setOver} = useStateContext()
     return(
         <aside onMouseOver={()=>setOver(true)}  className={over?`side-menu-display`:'side-menu-hide'}>
-          <div className="m-20">
-           <button className="text-amber-50 block">Some Item</button>
+          <div className="m-2 flex flex-col">
+           
           <div title="Close Menu" onClick={()=>setOver(false)} className="hover-close">
              
                <Close/>
           </div>
+          <CustomLink route="#" fontSize="20" inner="Somewere"></CustomLink>
           </div>
         </aside>
     )
