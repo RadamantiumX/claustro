3ca6444ccc6c5620ccdfcddb75e76ceb1e02c0c0
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-10-13 23:01:27 -0300

    feat: change name

--- loader/shop/src/components/buttons/MenuLinks.tsx
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom"
-import React from 'react';
-
-export const MenuLinks:React.FC<{children:React.ReactNode}> = ({children}) => {
-  return (
-    <Link className="link-hover" to="#">{children}</Link>
-  )
-}
