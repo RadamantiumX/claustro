4f4a607629ade3cb46b92213ca4cbe65ca1d73cd
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-10-13 23:58:13 -0300

    feat: some new changes

--- loader/shop/src/pages/default/Dashboard.tsx
+++ loader/shop/src/pages/default/Dashboard.tsx
@@ -7,7 +7,12 @@ export default function Dashboard():React.ReactNode {
  
   return (
     <div className="page-flex">
-      <h1 className="text-red-500">Dashboard</h1>
+      <h1 className="text-red-500">
+        <div className="mt-20">
+          8000 Users Colab Accounts
+          {/* Put inside all data and records counting */}
+        </div>
+      </h1>
     </div>
   )
 }
