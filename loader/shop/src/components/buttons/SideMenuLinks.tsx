59b5b08d95c39dc7e368f10db2bdbddfdb4624a8
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-10-13 23:57:35 -0300

    feat: new prop

--- loader/shop/src/components/buttons/SideMenuLinks.tsx
+++ loader/shop/src/components/buttons/SideMenuLinks.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const SideMenuLinks:React.FC<{children:React.ReactNode}> = ({children}) => {
+export const SideMenuLinks:React.FC<{children:React.ReactNode, path:string}> = ({children, path}) => {
     return(
         <>
-        <Link className="link-underline" to="#">{children}</Link>
+        <Link className="link-underline" to={path}>{children}</Link>
         </>
     )
 }
