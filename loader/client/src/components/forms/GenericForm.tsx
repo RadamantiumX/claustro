6cbabfa4a673e9cdd0e726b3068f132eff87acdd
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-10-21 15:23:53 -0300

    feat: adding new mocking input data

--- loader/client/src/components/forms/GenericForm.tsx
+++ loader/client/src/components/forms/GenericForm.tsx
@@ -1,43 +1,70 @@
 import React from 'react'
+import { FormButton } from '../buttons/FormButton'
 
+const inputs = [
+  {
+    for: "email-source",
+    label: "Email Source",
+    type: "email",
+    id: "email-source",
+    name: "email-source",
+    placeholder: "user@mail.com",
+  },
+  {
+    for: "email-password",
+    label: "Email Password",
+    type: "text",
+    id: "email-password",
+    name: "email-password",
+    placeholder: "Insert Email Password",
+  },
+  {
+    for: "x-username",
+    label: "X Username",
+    type: "text",
+    id: "x-username",
+    name: "x-username",
+    placeholder: "xusername",
+  },
+  {
+    for: "x-password",
+    label: "X Password",
+    type: "text",
+    id: "x-password",
+    name: "x-password",
+    placeholder: "Insert X Username Password",
+  },
+]
 
 export const GenericForm = ():React.ReactNode => {
+  const loading = false
   return (
    <>
       <div className='w-[50%]'>
             <form action="">
                <div className='flex flex-col gap-y-4'>
-
-                <div>
-                   <label htmlFor="email-source" className='flex flex-row items-center gap-3'>Email Source <div className="w-[70%]" >
+             {inputs.map((item, index)=>(
+              <div key={index}>
+                   <label htmlFor={item.for} className='flex flex-row items-center gap-3'>{item.label}<div className="w-[70%]" >
                 </div>
                 </label>
                  <div className="relative w-full">
                     <input
-                      type="text"
-                      id="id"
-                      name="name"
-                      placeholder="placeholder"
+                      type={item.type}
+                      id={item.id}
+                      name={item.name}
+                      placeholder={item.placeholder}
                       className="w-full p-4 pr-12 transition-all bg-transparent border rounded-lg outline-none border-slate-200 focus:border-blue-500"
                     /> 
                   </div>
                 </div>
+             ))}
+                
                
                
-              <div>
-                <label htmlFor="email-source-psw" className='flex flex-row items-center gap-3'>Email Source <div className="w-[70%]">
-                </div>
-                </label>
-                 <div className="relative w-full">
-                    <input
-                      type="text"
-                      id="id"
-                      name="name"
-                      placeholder="placeholder"
-                      className="w-full p-4 pr-12 transition-all bg-transparent border rounded-lg outline-none border-slate-200 focus:border-blue-500"
-                    /> 
-                  </div>
-              </div>
+        <FormButton loading={loading}>
+          Add new record
+        </FormButton>
                
                
                </div>
