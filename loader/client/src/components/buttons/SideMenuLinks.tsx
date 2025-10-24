2ae7c195ab4a171231fce617f77b4455a1ed9901
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-10-23 22:57:14 -0300

    feat: no relevants changes

--- loader/client/src/components/buttons/SideMenuLinks.tsx
+++ loader/client/src/components/buttons/SideMenuLinks.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useStateContext } from '../../hooks/useCtxStates';
 
 export const SideMenuLinks:React.FC<{children:React.ReactNode, path:string}> = ({children, path}) => {
-    const { setNotification } = useStateContext()
+    
     return(
         <>
-        <Link onClick={()=>setNotification('classdfasdasd')} className="link-underline" to={path}>{children}</Link>
+        <Link className="link-underline" to={path}>{children}</Link>
         </>
     )
 }
--- loader/client/src/components/cards/NotificationCard.tsx
+++ loader/client/src/components/cards/NotificationCard.tsx
@@ -1,21 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// import React, { useState, useEffect } from 'react'
-
-
+import { useStateContext } from "../../hooks/useCtxStates"
 
 export const NotificationCard = () => {
-  
-// TODO: make style to transition from the top
+  const {notification} = useStateContext()
 
   return (
-    <div className='flex justify-center'>
-    <div className='flex flex-col items-center bg-amber-50 w-[20%] absolute rounded-sm z-[1000] p-5 mt-10'>
-       
-       
-       <p>Notifacation Message! ⚠️</p>
+    <div className={'flex justify-center'}>
+    <div className='card-hide'>
+       <p className="text-gray-600">{notification} ⚠️</p>
        <div style={{ animation: `load 5s normal forwards` }} className="h-1 bg-amber-500"></div>
-       
-       
     </div>
     </div>
    
