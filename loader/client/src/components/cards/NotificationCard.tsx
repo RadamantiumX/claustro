95a026818e56f6870185ddf196c0c5a757851ce6
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-10-26 23:09:44 -0300

    feat: change the font size

--- loader/client/src/components/cards/NotificationCard.tsx
+++ loader/client/src/components/cards/NotificationCard.tsx
@@ -6,7 +6,7 @@ export const NotificationCard = () => {
   return (
     <div className={'flex justify-center'}>
     <div className={notification.match('Success')?'card-hide bg-green-600':'card-hide bg-red-600'}>
-       <p className="text-gray-300">{notification} ⚠️</p>
+       <p className="text-gray-300 text-[15px]">{notification}</p>
        <div style={{ animation: `load 5s normal forwards` }} className={"h-1 bg-amber-500"}></div>
     </div>
     </div>
