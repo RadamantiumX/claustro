728321704f7a089da50729ad0916cbc30b174b12
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-10-26 23:09:09 -0300

    fix: justifing loading state

--- loader/client/src/components/buttons/FormButton.tsx
+++ loader/client/src/components/buttons/FormButton.tsx
@@ -8,7 +8,7 @@ export const FormButton:React.FC<{children:React.ReactNode, loading:boolean}> = 
         <>
         <button type="submit" className={`rounded-sm bg-blue-400 px-3 py-1 font-bold ${loading ? "opacity-50":"cursor-pointer"}`} disabled={loading}>
             {// Change inner button content --> State depending
-                              !loading ? children : <div className="flex flex-row items-center gap-x-5"><Loader/> Loading...</div>
+                              !loading ? children : <div className="flex flex-row justify-center gap-x-5"><Loader/> Loading...</div>
             }
         </button>
         </>
