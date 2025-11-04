97f6358bb8f145cdbf0c526aa7eec9e170c51dec
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-11-04 20:02:02 -0300

    feat: adding confirmation card into add page

--- loader/client/src/components/cards/ConfirmationCard.tsx
+++ loader/client/src/components/cards/ConfirmationCard.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-export const ConfirmationCard = (legend:string):React.ReactNode => {
+export interface CardConfirmProps {
+  legend:string;
+
+}
+
+export const ConfirmationCard:React.FC<CardConfirmProps> = ({legend}):React.ReactNode => {
   return (
     <>
    <div className={'flex justify-center'}>
      <div className={'card-hide bg-amber-600'}>
        <p className="text-gray-300 text-[15px]">{legend}</p>
         <div className='flex flex-row gap-x-5'>
-          <button className='rounded-sm px-2 bg-green-700 cursor-pointer'>Yes</button> 
-          <button className='rounded-sm px-2 bg-red-700 cursor-pointer'>No</button>
+          <button  className='rounded-sm px-2 bg-green-700 cursor-pointer'>Yes</button> 
+          <button  className='rounded-sm px-2 bg-red-700 cursor-pointer'>No</button>
         </div>
      </div>
     </div>
--- loader/client/src/pages/default/Add.tsx
+++ loader/client/src/pages/default/Add.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import HeroSection from '../../features/default/add/HeroSection'
 import FormSection from '../../features/default/add/FormSection'
-
+import { ConfirmationCard } from '../../components/cards/ConfirmationCard'
 
 export default function Add():React.ReactNode {
   return (
     <div className="page-flex">
+      <ConfirmationCard legend='Are you sure?'/>
       <HeroSection/>
       <FormSection/>
     </div>
