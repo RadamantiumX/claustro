e1ce999dfe564691ce177d7bcb5ca7971af22ab1
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-11-07 23:28:36 -0300

    feat: new hook

--- loader/client/src/hooks/useReload.ts
+++ loader/client/src/hooks/useReload.ts
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect } from "react"
+import { useStateContext } from "./useCtxStates"
 
 export const useReload = () => {
-    const [ reload, setReload ] = useState(false)
+   const { inputError } = useStateContext()
+    const handleBeforeUnload = (e:any) =>{
+      if(inputError.length > 0){
+        e.preventDefault()
+        e.returnValue = "Invalid Form data"
+      }
+    }
      useEffect(()=>{
-        window.addEventListener("beforeunload", ()=>{
-           window.confirm('Are you sure?')
-        })
-        return  window.addEventListener("beforeunload", ()=>{
-          console.log('reload page')
-          console.log(reload)
-          setReload(true)
-        })
-    },[reload])
+        window.addEventListener("beforeunload",handleBeforeUnload)
+        return ()=>{
+          window.addEventListener("beforeunload",handleBeforeUnload)
+        } 
+    },[inputError])
 
     
 }
