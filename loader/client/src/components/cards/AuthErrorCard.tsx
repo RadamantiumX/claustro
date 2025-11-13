96eb0ae83fac159d404516ff97ebce05fec5fb01
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-11-12 23:03:21 -0300

    feat: adding new prop

--- loader/client/src/components/cards/AuthErrorCard.tsx
+++ loader/client/src/components/cards/AuthErrorCard.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export const AuthErrorCard = () => {
+export const AuthErrorCard:React.FC<{message:string}> = ({message}):React.ReactNode => {
   return (
     <>
      <div className={'flex justify-center relative w-[90%]'}>
      <div className={'card-over-index bg-red-600'}>
-       <p className="text-gray-300 text-[12px]">Some ERROR</p>
+       <p className="text-gray-300 text-[12px]">{message}</p>
         
      </div>
     </div>
