35289c0736618fc5fd0aa17ad727efbc93740071
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-11-19 23:31:13 -0300

    feat: clear search params

--- loader/client/src/components/misc/SearchQuery.tsx
+++ loader/client/src/components/misc/SearchQuery.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
-
+import { Close } from '../../icons/icons';
+import { Link } from 'react-router-dom';
+import { useSearchData } from '../../hooks/useSearchData';
 
 export const SearchQuery:React.FC<{query:string | null}> = ({query}) => {
+  const { setSearchData } = useSearchData()
   return (
     <div className='w-[70%]'>
-        <div className='flex justify-start'>
+        <div className='flex flex-row gap-2 justify-start items-center'>
         <h3 className='text-2xl text-gray-400 italic'>Results of: {query}</h3>
+        <Link to="/" onClick={()=>{ setSearchData([]) }} className='hover:text-red-500' title='Remove filter'><Close/></Link>
         </div>
     </div>
   )
