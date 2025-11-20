c808ce7d9ff9fba11e4286237855e4e19dca3d94
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-11-19 23:30:29 -0300

    feat: testing

--- loader/client/src/hooks/useGetParams.ts
+++ loader/client/src/hooks/useGetParams.ts
@@ -1,8 +1,7 @@
 import { useSearchParams } from "react-router-dom"
 
 export const useGetParams = () => {
-  const [searchParams] = useSearchParams()
+  const [ searchParams ] = useSearchParams()
   const searchP = searchParams.get('search')
-  console.log(searchP)
   return { searchP }
 }
