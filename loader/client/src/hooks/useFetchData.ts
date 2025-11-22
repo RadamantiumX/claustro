a8edba45209b8e7069f94b9899e466a6d81c3dd8
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-11-21 23:29:32 -0300

    feat: adding content on effect

--- loader/client/src/hooks/useFetchData.ts
+++ loader/client/src/hooks/useFetchData.ts
@@ -1,15 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery } from "@tanstack/react-query";
 import { useTRPC } from "../utils/trpc";
-
-
+import { useStateContext } from "./useCtxStates";
+import { useEffect } from "react";
 
 // TODO: finish this mutation
 export const useFetchData = () => {
     const trpc = useTRPC()
-   
-    const allData = useQuery(trpc.data.list.queryOptions())
-    const data = allData.data
-
-
-   return { data }
+    const { setData } = useStateContext()
+    
+    const queryRequest = useQuery(trpc.data.list.queryOptions())
+    const allData:any = queryRequest.data
+    useEffect(()=>{
+        setData(allData)
+    },[allData])
+   return {  }
 }
