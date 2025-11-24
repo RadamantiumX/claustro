192cd0fccc36439da9a7f9383790c40328cd4e50
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-11-24 15:41:57 -0300

    feat: change to mutation for send pagination parameters

--- loader/client/src/hooks/useFetchData.ts
+++ loader/client/src/hooks/useFetchData.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { useTRPC } from "../utils/trpc";
 import { useStateContext } from "./useCtxStates";
 import { useEffect } from "react";
@@ -7,12 +7,25 @@ import { useEffect } from "react";
 // TODO: finish this mutation
 export const useFetchData = () => {
     const trpc = useTRPC()
-    const { setData } = useStateContext()
+    const { setData }:any = useStateContext()
     
-    const queryRequest = useQuery(trpc.data.list.queryOptions())
-    const allData:any = queryRequest.data
+    const queryRequest = useMutation(trpc.data.list.mutationOptions())
     useEffect(()=>{
-        setData(allData)
-    },[allData])
-   return {  }
+        try{
+      queryRequest.mutate({page:1, pageSize:5},{
+        onSuccess:(data, variables)=>{
+            console.log(variables)
+          setData(data)
+        },
+        onError:(error)=>{
+          console.log(error)
+        }
+      })
+    }catch(error){
+      console.log(error)
+    }
+    },[])
+    
+
+   return
 }
