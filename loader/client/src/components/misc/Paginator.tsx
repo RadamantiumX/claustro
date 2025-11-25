247de4a9667378ea8d45e0228199a8d2c32e25d0
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-11-25 12:59:09 -0300

    feat: setting paginator model

---
+++ loader/client/src/components/misc/Paginator.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { ChevronLeft, ChevronRight } from '../../icons/icons';
+
+export const Paginator = ():React.ReactNode => {
+  return (
+    <>
+      <nav>
+        <div className='flex flex-row gap-2'>
+            <Link className='hover:text-amber-600' to="#"><ChevronLeft/></Link>
+            <Link className='hover:text-amber-600 focus:text-amber-600' to="">1</Link>
+            <Link className='hover:text-amber-600 focus:text-amber-600' to="">2</Link>
+            <Link className='hover:text-amber-600' to="#"><ChevronRight/></Link>
+        </div>
+      </nav>
+    </>
+  )
+}
