1dbc623cbeed32e402a6b112ff7a91e519b0aefb
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-12-03 23:34:13 -0300

    feat: almost finish pagination

--- loader/client/src/components/misc/Paginator.tsx
+++ loader/client/src/components/misc/Paginator.tsx
@@ -10,7 +10,7 @@ export const Paginator = ():React.ReactNode => {
   const [ searchParams, setSearchParams ]:any = useSearchParams()
   const [ currentPage, setCurrentPage ] = useState(1)
   const [start, setStart] = useState(0)
-  const [end, setEnd] = useState(0)
+  const [end, setEnd] = useState(5)
   const [startPrev, setStartPrev] = useState(0)
   const [endPrev, setEndPrev] = useState(0)
   // const end = (currentPage * pageSize) /pageSize + pageSize - 1
@@ -40,13 +40,18 @@ export const Paginator = ():React.ReactNode => {
 
 
   useEffect(()=>{
-    if(currentPage >= 5 ){
-      setEnd((currentPage * pageSize)/ pageSize + pageSize -1)
-     setStart(end - pageSize)
-    }else{
+    
+    if(currentPage <= 5){
       setEnd(5)
       setStart(0)
     }
+    
+   
+      setEnd((currentPage * pageSize)/ pageSize + pageSize -1)
+      setStart(end - pageSize)
+    
+    
+
     const numPage = parseInt(searchParams.get("page")) || 1
     setSearchParams({page:currentPage.toString()})
     setCurrentPage(numPage)
@@ -58,10 +63,13 @@ export const Paginator = ():React.ReactNode => {
         <div className='flex flex-row gap-2'>
             
             {searchParams.get("page") !== "1" && <button onClick={()=>handleChangePage("prev")}  className='hover:text-amber-600 cursor-pointer'><ChevronLeft/></button>}
-            {currentPage > 5 && arrayPages.slice(0,4).map((item, key)=>(
+            {currentPage >= 3 && arrayPages.slice(0,2).map((item, key)=>(
               <button key={key} onFocus={()=>setCurrentPage(searchParams.get("page"))} onClick={()=>handleChangeState(item.toString())} className={`text-2xl cursor-pointer hover:text-amber-600 visited:text-amber-600 ${item.toString() === searchParams.get("page") && 'border rounded-sm border-amber-600 px-2 py-[1px]'}`}>{item}</button>
             ))}
-            {currentPage >= 9 && <button><Dots/></button>}
+            {currentPage > 4 && <button><Dots/></button>}
+            {currentPage > 3 && arrayPages.slice((start - 1),(start)).map((item, key)=>(
+              <button key={key} onFocus={()=>setCurrentPage(searchParams.get("page"))} onClick={()=>handleChangeState(item.toString())} className={`text-2xl cursor-pointer hover:text-amber-600 visited:text-amber-600 ${item.toString() === searchParams.get("page") && 'border rounded-sm border-amber-600 px-2 py-[1px]'}`}>{item}</button>
+            ))}
             {arrayPages.slice(start,end).map((item, key)=>(
               <button key={key} onFocus={()=>setCurrentPage(searchParams.get("page"))} onClick={()=>handleChangeState(item.toString())} className={`text-2xl cursor-pointer hover:text-amber-600 visited:text-amber-600 ${item.toString() === searchParams.get("page") && 'border rounded-sm border-amber-600 px-2 py-[1px]'}`}>{item}</button>
             ))}
