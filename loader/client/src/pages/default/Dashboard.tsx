ac44b30f571ac1a27eed82fcfa4f520d4ba0eca8
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-12-07 02:30:31 -0300

    feat: some test

--- loader/client/src/pages/default/Dashboard.tsx
+++ loader/client/src/pages/default/Dashboard.tsx
@@ -1,17 +1,26 @@
 import React from "react"
 import HeroSection from "../../features/default/dashboard/HeroSection"
 import DataSection from "../../features/default/dashboard/DataSection"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom"
+
 /**
  * Dashboard page on Default Layout
  * @returns {React.ReactNode}
  */
+type ParamTuple = [string, string]
+
 export default function Dashboard():React.ReactNode {
   const [ searchParams, setSearchParams ] = useSearchParams()
+  const [ arrayParams, setArrayParams ] = useState<ParamTuple[]>([])
+  const cluster:ParamTuple = ["cluster", "rival"]
+  const lan:ParamTuple = ["lan","local"]
+
   useEffect(()=>{
-    setSearchParams({ some:["lipsum", "dolor"] })
+    setArrayParams([...arrayParams,cluster,lan])
+    
   },[])
+  setSearchParams([...searchParams, cluster, lan])
   return (
     <div className="page-flex">
       <HeroSection/>
