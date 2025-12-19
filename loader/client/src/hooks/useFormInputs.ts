49ff439dbcf94088de4af38336b249624a2b5c02
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-12-19 11:42:25 -0300

    feat: adding comments

--- loader/client/src/hooks/useFormInputs.ts
+++ loader/client/src/hooks/useFormInputs.ts
@@ -3,19 +3,22 @@ import type { FormDataAddData } from "../types/hooks";
 import type { FormDataSignIn } from "../types/hooks";
 import type { InputArray } from "../types/hooks";
 
-
+/**
+ * Inputs fields generation with Array fields CONSTANTS
+ * @param inputsArray Array with custom fields (values empty)
+ * @param formData All the fields values 
+ * @returns Array with custom fields + All the fields values 
+ */
 export const useFormInputs = (inputsArray:InputArray[] | Omit<InputArray, "for"| "label">[], formData:FormDataAddData | FormDataSignIn) => {
    const [formInputs, setFormInputs] = useState(inputsArray)
   
    useMemo(()=>{
   
     formInputs.map((data, index) =>{
-        console.log(data)
-        console.log(Object.values(formData)[index])
-        Object.assign(data, {value: Object.values(formData)[index]})
+        Object.assign(data, {value: Object.values(formData)[index]}) // Assign values
      })
      setFormInputs(formInputs)
-     // console.log(formInputs)
+    
     },
  [inputsArray, formData])
 
