feed50f92e68f954d95d18a9583d9e453dd3e4da
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-12-20 22:46:26 -0300

    feat: using the hook

--- loader/client/src/features/default/index/record/FormSection.tsx
+++ loader/client/src/features/default/index/record/FormSection.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import DefaultContent from '../../../../components/sectionsTemplate/DefaultContent'
+import { useFetchUniqueData } from '../../../../hooks/hooks';
+
 
 export default function FormSection():React.ReactNode {
+  const { relatedData } = useFetchUniqueData()
   return (
     <DefaultContent>
         <div>Some</div>
