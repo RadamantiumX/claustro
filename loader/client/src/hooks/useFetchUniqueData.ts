32503bc4429a6187c4851424c65507e0f5ae56d3
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-12-20 22:45:38 -0300

    feat: new hook for nested data

--- loader/client/src/hooks/useFetchUniqueData.ts
+++ loader/client/src/hooks/useFetchUniqueData.ts
@@ -1,8 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useTRPC } from "../utils/trpc";
 import { useMutation } from "@tanstack/react-query";
-import { useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export const useFetchUniqueData = () => {
-   const [ searchParams ] = useSearchParams()
-   
+   const trpc = useTRPC()
+   const params:any = useParams()
+   const [relatedData, setRelatedData] = useState([])
+   const mutationRq = useMutation(trpc.data.selectForId.mutationOptions())
+   console.log(params.id)
+   useEffect(()=>{
+       try{
+      mutationRq.mutate({id:parseInt(params.id)},{
+      onSuccess:(data, variables)=>{
+         console.log(data)
+         console.log(variables)
+      },
+      onError:(error)=>{
+         console.log(error)
+      }
+     })
+   }catch(error){
+     console.log(error)
+   }
+   },[])
+  
+
+   return { relatedData }
 }
