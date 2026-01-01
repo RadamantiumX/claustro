abda90a53f72fb9918bc3c64fa8699d9866aa27b
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-01-01 00:33:58 -0300

    feat: fixing new type

--- loader/client/src/hooks/custom/useFormData.ts
+++ loader/client/src/hooks/custom/useFormData.ts
@@ -1,11 +1,11 @@
 import { useState, useMemo} from "react"
-import type { DataValues } from "../../types/hooks"
+// import type { DataValues } from "../../types/hooks"
 
 
 
 // TODO: adding other types of values on Generic Type
 export const useFormData = <T>(values:T) =>{
-    const [ formData, setFormData ] = useState<T>({})
+    const [ formData, setFormData ] = useState<T>({} as T) // TS forced to accept {}
     
 useMemo(()=>{
     setFormData(values)
