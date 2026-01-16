a5f73adcf2512e9d9c0ee6c120931effe1c552c9
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-01-16 14:33:40 -0300

    feat: comments

--- loader/client/src/components/buttons/UserMenuLinks.tsx
+++ loader/client/src/components/buttons/UserMenuLinks.tsx
@@ -2,6 +2,11 @@ import { Link } from "react-router-dom"
 import React from 'react';
 import { useStateContext } from "../../hooks/hooks";
 
+
+/**
+ * Only for User Menu Side Bar
+ *  @children - {React.ReactNode}
+ */
 export const UserMenuLinks:React.FC<{children:React.ReactNode}> = ({children}) => {
   const { user } = useStateContext()
   return (
