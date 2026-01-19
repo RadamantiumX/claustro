c7f9ea6d01f582a8578fe8aac375da7ef0cfaf4e
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-01-18 23:57:39 -0300

    feat: adding barrel

---
+++ loader/api/src/schemas/index.ts
@@ -0,0 +1,5 @@
+export { apiDataSchema } from './zodSchemas/apiDataValidation';
+export { apiKeySchema } from './zodSchemas/apiKeyValidation';
+export { dataSchema, metaSchema } from './zodSchemas/dataValidation';
+export { refreshTokenSchema } from './zodSchemas/refreshTokenValidation';
+export { userSchema, newPasswordSchema  } from './zodSchemas/userColabValidation';
