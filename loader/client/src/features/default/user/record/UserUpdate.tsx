a5e7255c8a03e3c3879505a0ae77c6cea35b319e
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-01-23 16:08:40 -0300

    feat: adding ternary to avoid null or undefined values

--- loader/client/src/features/default/user/record/UserUpdate.tsx
+++ loader/client/src/features/default/user/record/UserUpdate.tsx
@@ -17,7 +17,7 @@ export const UserUpdate = ({userData}:UserSetting):React.ReactNode => {
           <form action="">
             <div className='grid grid-cols-2 place-items-start gap-4'>
               <label htmlFor="user" className="block text-sm/6 font-medium text-white"><span className='align-middle'>Username</span></label>
-            <input id="user" type="text" value={userData.username} disabled className='flex-none justify-items-start rounded-md bg-gray-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-gray-200 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 isabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20' />
+            <input id="user" type="text" value={userData ? userData.username : "Loading..."} disabled className='flex-none justify-items-start rounded-md bg-gray-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-gray-200 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 isabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20' />
             </div>
             
           </form>
