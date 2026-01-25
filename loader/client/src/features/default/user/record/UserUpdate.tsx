a2c7a26da2415a1a5c2795be15236553f4bd4981
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-01-25 16:30:30 -0300

    feat: some changes

--- loader/client/src/features/default/user/record/UserUpdate.tsx
+++ loader/client/src/features/default/user/record/UserUpdate.tsx
@@ -25,7 +25,7 @@ export const UserUpdate = ({userData}:UserSetting):React.ReactNode => {
           <form action="">
             <div className='flex items-center gap-x-4'>
               <label htmlFor="user" className="block text-sm/6 font-medium text-white"><span className='font-bold'>Email</span></label>
-            <input id="user" type="text" value={userData?.email ? userData?.email : ""} disabled={show} className='flex-none justify-items-start rounded-md bg-gray-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-gray-200 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 isabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 disabled:shadow-none dark:disabled:border-gray-700 dark:disabled:bg-gray-800/20' />
+            <input id="user" type="text" value={userData?.email ? userData?.email : ""} disabled={show} className='single-input' />
              {!show && <button className='text-[12px] bg-green-500 rounded-md p-2 cursor-pointer'>Change</button> }
             </div>
           </form>
