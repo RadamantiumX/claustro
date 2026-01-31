11cc122633e4e084df08899e61a508e1ecbb5bb0
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-01-31 20:30:22 -0300

    feat: password update comp

--- loader/client/src/features/default/user/record/PasswordUpdate.tsx
+++ loader/client/src/features/default/user/record/PasswordUpdate.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { GenericForm } from '../../../../components/forms/GenericForm';
+import { useMutationHandler, useFormInputs } from '../../../../hooks/hooks';
+import { PASSWORD_UPDATE } from '../../../../utils/const';
 
 export const PasswordUpdate = ():React.ReactNode => {
+  const formValues = {
+    password:"",
+    newPassword:"",
+    retypeNewPassword:"",
+    id:""
+  }
+  const { formData, handleChange, handleSubmit } = useMutationHandler(formValues, {route:"userColab",method:"updatePassword"})
+  const { formInputs } = useFormInputs(PASSWORD_UPDATE,formData)
   return (
     <>
+    <GenericForm handleChange={handleChange} handleSubmit={handleSubmit} dataInputs={formInputs} innerTextButton='Change Password'/>
     </>
   )
 }
