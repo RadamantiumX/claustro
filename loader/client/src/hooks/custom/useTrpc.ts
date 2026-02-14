d4563fc095fa4d0cf00dcaef3ffba7c897aa5890
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-02-14 20:07:32 -0300

    feat: reminder message

--- loader/client/src/hooks/custom/useTrpc.ts
+++ loader/client/src/hooks/custom/useTrpc.ts
@@ -85,3 +85,4 @@ export const useTrpc = () => {
 
 }
 
+// TODO: <<REMINDER>> --> If you more support about this, please enter in this URL: https://www.google.com/search?q=trpc+refresh+token+react+example&sca_esv=19111bd9c387aca8&sxsrf=ANbL-n6jp3Sv9UVamGtmeRN7QN4rHBGI_A:1771108566514&ei=1viQaduLH7HU1sQPsqyJkAQ&start=0&sa=N&sstk=Af77f_dcm6TntcM54tnsjCMLs6RioaliXF4CkCsSb0-mxzC_bgJhDJ3butGQwbO5miRSDkPxbMDuC5vmwB0iq5-gAMmlIYqYhcsf9eoQ-DWIthbkDkY_vmUKoKEnL5YhsRSG&ved=2ahUKEwjbztzHhdqSAxUxqpUCHTJWAkI4ChDy0wN6BAgLEAQ&biw=1242&bih=545&dpr=1.1
