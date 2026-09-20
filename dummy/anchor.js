95210df688c679cce962be13ba169037edbe012e
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-09-20 03:00:31 -0300

    feat: can be works

--- dummy/anchor.js
+++ dummy/anchor.js
@@ -1,27 +1,39 @@
 import {spawn} from 'node:child_process'
 
+const io = []
+
 export async function npxCli() {
   
     try{
-        let io = []
+        
         const turbo = spawn(`npx turbo ls --output=json`,
             {
                 shell:true,
-                stdio: ['overlapped'],
+                stdio: ['inherit', 'pipe', 'pipe', 'overlapped'],
             }
         )
       
 
-        turbo.stdout.on('data', (chunks)=>{
-         
-         io.push(chunks)    
+        turbo.stdout.on('data', (data)=>{
+         const parsed = JSON.parse(data)
+         parsed.packages.items.map((item)=>{
+            io.push(item)
+         })
+         console.log(parsed.packages.items)
          })
 
          turbo.stderr.on('error', (error)=>{
             console.error(error.message)
             
          })
-     return {turbo, io}
+
+         turbo.on('close', (code)=>{
+            if(code !== 0){
+                console.error('Error on process')
+            }
+            return
+         })
+     return
     //    turbo.on('close', (code)=>{
     //          if(code !== 0){
     //         console.error('Error on process')
@@ -41,19 +53,45 @@ export async function npxCli() {
     
 }
 
-const { turbo, io } = await npxCli()
+npxCli()
+console.log(io)
+// console.log(io)
+// const { turbo, io } = await npxCli()
+// async function clousure(jsonChunk){
+//     try{
+//     const fullBuff =  Buffer.concat(jsonChunk)
+//      const jsonString = new TextDecoder('utf-8').decode(fullBuff)
+//      const parsedObj = await JSON.parse(jsonString)
+//      console.log(parsedObj.packages.items)
+//      return parsedObj
 
-turbo.on('close',cbTurbo)
+//     }catch(error){
+//         console.error(`An error appear on: ${error}`)
+//         return
+//     }
+  
+// }
+// await clousure(io)
+
+// turbo.on('close',(code)=>{
+//     if(code !== 0){
+//         console.error('Error on process')
+//     }
+    
+// })
 
 
-function cbTurbo(code){
-  if(code !== 0) {
-    console.error('Error on process')
-    return
-  }
-     const fullBuff = Buffer.concat(io)
-     const jsonString = new TextDecoder('utf-8').decode(fullBuff)
-     const parsedObj = JSON.parse(jsonString)
-     console.log(parsedObj.packages.items)
-     return parsedObj
-}
+
+
+
+// function cbTurbo(code){
+//   if(code !== 0) {
+//     console.error('Error on process')
+//     return
+//   }
+//      const fullBuff = Buffer.concat(io)
+//      const jsonString = new TextDecoder('utf-8').decode(fullBuff)
+//      const parsedObj = JSON.parse(jsonString)
+//      console.log(parsedObj.packages.items)
+//      return parsedObj
+// }
